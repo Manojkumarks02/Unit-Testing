661cc55ce390ee6bac652250cc960690c42c1d71
Manojkumarks02 <manojkumarks063@gmail.com>
2024-10-08 16:48:11 +0530

    changes

--- src/test/java/com/unitTesting/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/unitTesting/repository/EmployeeRepositoryTest.java
@@ -1,14 +1,62 @@
 package com.unitTesting.repository;
 
+
+import com.unitTesting.entity.Employee;
+import com.unitTesting.service.TestContainerConfiguration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-public class EmployeeRepositoryTest {
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(TestContainerConfiguration.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private Employee employee;
+
+    @BeforeEach
+    void setUp() {
+        employee = Employee.builder()
+                .id(1L)
+                .name("Anuj")
+                .email("anuj@gmail.com")
+                .salary(100L)
+                .build();
+    }
 
     @Test
-    void TestFindByEmail_whenEmailIsValid_thenReturnEmployee(){
+    void testFindByEmail_whenEmailIsPresent_thenReturnEmployee() {
+//        Arrange, Given
+        employeeRepository.save(employee);
 
+//        Act, When
+        List<Employee> employeeList = employeeRepository.findByEmail(employee.getEmail());
+
+//        Assert, Then
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList).isNotEmpty();
+        assertThat(employeeList.get(0).getEmail()).isEqualTo(employee.getEmail());
+    }
+
+    @Test
+    void testFindByEmail_whenEmailIsNotFound_thenReturnEmptyEmployeeList() {
+//        Given
+        String email = "notPresent.123@gmail.com";
+//        When
+        List<Employee> employeeList = employeeRepository.findByEmail(email);
+//        Then
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList).isEmpty();
     }
 }
-
-
---
+++ src/test/java/com/unitTesting/service/TestContainerConfiguration.java
@@ -0,0 +1,15 @@
+package com.unitTesting.service;
+
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class TestContainerConfiguration {
+
+//    @Bean
+//    @ServiceConnection
+//    PostgreSQLContainer<?> postgresContainer() {
+//        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+//    }
+}
